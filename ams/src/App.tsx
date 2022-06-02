6426258133762f56df9326897c551ef534a91e17
Manoj Mundekkottil(UST <200291@ust-global.com>
2022-06-03 00:28:55 +0530

    added header bar and modules

--- ams/src/App.tsx
+++ ams/src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Article from './components/views/article/article';
+import Navbar from './components/common/navbar';
 
 function App() {
   return (
     <div className="App">
+      <><Navbar/></>        
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          AMS
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Article></Article>
       </header>
     </div>
   );
---
+++ ams/src/components/views/article/article.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import {Button} from '@material-ui/core'
+
+const Article: React.FC<any> = ()=>{
+    return ( <div className="Article">
+      Article Page 
+      {/* <Button> Press me </Button> */}
+      </div>);
+}
+export default Article;
